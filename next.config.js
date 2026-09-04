64ca09cd3fb460e989bc3089c48cd996671637d6
T4TV <alleyway2027@gmail.com>
2026-09-04 13:20:00 -0500

    Add Prisma schema, TypeScript config, and environment template

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.cloudflarestorage.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
+
+module.exports = nextConfig;
